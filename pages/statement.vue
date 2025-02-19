7a2e2332fc961f887c6f09f7213d277d20dab5ab
Andreas Hocevar <andreas.hocevar@gmail.com>
2025-02-19 10:38:44 +0100

    Reinrassige Zuchttiere

--- pages/statement.vue
+++ pages/statement.vue
@@ -15,7 +15,7 @@ const { mdAndUp, xs } = useDisplay();
     <v-row>
       <v-col :cols="mdAndUp ? 4 : xs ? 12 : 6">
         <v-card>
-          <v-card-title class="d-flex justify-center">Soja</v-card-title>
+          <v-card-title class="d-flex justify-center">Sojabohnen</v-card-title>
           <v-card-text class="d-flex justify-center"
             ><v-icon :icon="mdiSprout" size="50"
           /></v-card-text>
@@ -26,7 +26,7 @@ const { mdAndUp, xs } = useDisplay();
       </v-col>
       <v-col :cols="mdAndUp ? 4 : xs ? 12 : 6">
         <v-card>
-          <v-card-title class="d-flex justify-center">Rind</v-card-title>
+          <v-card-title class="d-flex justify-center">Rind (reinrassige Zuchttiere)</v-card-title>
           <v-card-text class="d-flex justify-center"
             ><v-icon :icon="mdiCow" size="50"
           /></v-card-text>
@@ -37,23 +37,24 @@ const { mdAndUp, xs } = useDisplay();
       </v-col>
       <v-col :cols="mdAndUp ? 4 : xs ? 12 : 6">
         <v-card>
-          <v-card-title class="d-flex justify-center">Wald</v-card-title>
+          <v-card-title class="d-flex justify-center">Rind (andere)</v-card-title>
           <v-card-text class="d-flex justify-center"
-            ><v-icon :icon="mdiForestOutline" size="50"
+            ><v-icon :icon="mdiCow" size="50"
           /></v-card-text>
           <v-card-actions class="d-flex justify-center"
             ><v-btn color="primary" :prepend-icon="mdiPlus">Hinzufügen</v-btn></v-card-actions
           >
         </v-card>
       </v-col>
-      <v-col cols="12">
-        <v-card style="height: 500px; width: 100%">
-          <v-layout class="fill-height">
-            <v-app-bar flat density="compact" title="Sorgfaltspflichterklärung"></v-app-bar>
-            <v-main>
-              <agraratlas-map class="fill-height" />
-            </v-main>
-          </v-layout>
+      <v-col :cols="mdAndUp ? 4 : xs ? 12 : 6">
+        <v-card>
+          <v-card-title class="d-flex justify-center">Rohholz </v-card-title>
+          <v-card-text class="d-flex justify-center"
+            ><v-icon :icon="mdiForestOutline" size="50"
+          /></v-card-text>
+          <v-card-actions class="d-flex justify-center"
+            ><v-btn color="primary" :prepend-icon="mdiPlus">Hinzufügen</v-btn></v-card-actions
+          >
         </v-card>
       </v-col>
     </v-row>
