5c190eb48452226c8a034bfe73ed1cc0e763dcb2
Andreas Hocevar <andreas.hocevar@gmail.com>
2025-07-04 12:09:21 +0200

    [temp] More AMA login logging

--- server/routes/auth/ama.get.js
+++ server/routes/auth/ama.get.js
@@ -7,6 +7,7 @@ export default defineEventHandler(async (event) => {
     secure: true,
     expires: new Date(Date.now() + 60000),
   });
+  console.log('Redirecting to AMA login with cid:', cid);
   return sendRedirect(
     event,
     `https://login.ama.gv.at/amaloginserver/#/?src=ps_eudr_bml&app=ps_eudr_bml&cid=${cid}`,
